d527aa9b1b6af1d19119c3d1dd110ba989afdf3b
Devin Sag <devin.sag@architect.io>
2022-11-09 12:11:56 -0500

    init

---
+++ src/test/java/com/architect/springboot/ApiControllerTest.java
@@ -0,0 +1,24 @@
+package com.architect.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiControllerTest {
+
+	@Autowired
+	private TestRestTemplate template;
+
+    @Test
+    public void getApiGreeting() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/health", String.class);
+        assertThat(response.getBody()).isEqualTo("UP");
+    }
+}
